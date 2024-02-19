e5a6cdca5c560c8a557373a5728ded2448981262
naoyuki <kamaboko912@gmail.com>
2024-02-19 10:51:42 +0900

    appからapi削除

--- features/recipe/components/RecipeWrapper.tsx
+++ features/recipe/components/RecipeWrapper.tsx
@@ -4,15 +4,14 @@ import RecipeTitle from './RecipeTitle'
 
 const RecipeWrapper = async () => {
     const todayRanking = await getTodayRanking()
-    console.log(todayRanking.result[2].recipeTitle)
     return (
         <>
-            <RecipeThumbnail
+            {/* <RecipeThumbnail
                 foodImageUrl={todayRanking.result[2].foodImageUrl}
-            />
-            <div className="m-[8px]">
+            /> */}
+            {/* <div className="m-[8px]">
                 <RecipeTitle title={todayRanking.result[2].recipeTitle} />
-            </div>
+            </div> */}
         </>
     )
 }
