cefe3d33c3884a075760469fc4e552a3810512fb
naoyuki <kamaboko912@gmail.com>
2024-02-19 09:42:23 +0900

    todayRecipeがない場合はloadingという文字列を表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
 import RecipeThumbnail from '@/features/recipe/components/RecipeThumbnail'
 import RecipeTitle from '@/features/recipe/components/RecipeTitle'
 import RecipeDetailButton from '@/features/recipe/components/RecipeDetailButton'
-import { getTests } from '@/features/test/api/getTests'
 import { getCategoryName, getTodayRecipe } from './utils/recipe'
 
 export default async function Home() {
-    // const tests = await getTests()
     const todayRecipe = await getTodayRecipe()
     const categoryName = getCategoryName()
-    // console.log(todayRecipe)
+    if (!todayRecipe) return <div>loading...</div>
     return (
         <div className="m-[16px]">
-            {/* <div>{tests && <p>{tests[0].text}</p>}</div> */}
             <h1 className="mb-[16px] text-[32px] font-bold text-gray-700">
                 今日作る{categoryName}は・・・
             </h1>
