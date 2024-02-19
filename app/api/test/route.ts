e0b7e27f9cae79fb974be11b6244334dfee9cd0e
naoyuki <kamaboko912@gmail.com>
2024-02-19 10:40:54 +0900

    prismaのimport文修正

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,5 @@
 import { Test } from '@prisma/client'
-import prisma from '../../lib/prisma'
+import prisma from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
