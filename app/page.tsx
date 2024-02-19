d2c5fb2804cc2a3465c9f0e7ac2ad9652f9a7d9c
naoyuki <kamaboko912@gmail.com>
2024-02-19 14:02:36 +0900

    レシピ関連のコードを更新しました

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import RecipeDetailButton from '@/features/recipe/components/RecipeDetailButton'
 import RecipeWrapper from '@/features/recipe/components/RecipeWrapper'
 import RecipeHeader from '@/features/recipe/components/RecipeHeader'
 import { Suspense } from 'react'
+import { getTodayRecipe } from '@/utils/recipe'
 
 export default async function Page() {
+    const todayRanking = await getTodayRecipe()
+    const todayRecipe = todayRanking?.result[0]
+    console.log(todayRecipe)
     return (
         <div className="m-[16px]">
             <RecipeHeader />
--- features/recipe/components/RecipeWrapper.tsx
+++ features/recipe/components/RecipeWrapper.tsx
@@ -1,16 +1,14 @@
-import { getTodayRecipe } from '@/utils/recipe'
 import RecipeThumbnail from './RecipeThumbnail'
 import RecipeTitle from './RecipeTitle'
 
 const RecipeWrapper = async () => {
-    const todayRecipe = await getTodayRecipe()
     return (
         <>
-            <RecipeThumbnail
+            {/* <RecipeThumbnail
                 foodImageUrl={todayRecipe!.result[0].foodImageUrl}
-            />
+            /> */}
             <div className="m-[8px]">
-                <RecipeTitle title={todayRecipe!.result[0].recipeTitle} />
+                <RecipeTitle title="test" />
             </div>
         </>
     )
