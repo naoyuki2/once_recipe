0cbc82597cb3c90ca5de5e7dffbab8a619159d11
naoyuki <kamaboko912@gmail.com>
2024-02-19 09:56:00 +0900

    非同期処理をコメントアウト

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,17 @@ import RecipeDetailButton from '@/features/recipe/components/RecipeDetailButton'
 import { getCategoryName, getTodayRecipe } from './utils/recipe'
 
 export default async function Home() {
-    const todayRecipe = await getTodayRecipe()
+    // const todayRecipe = await getTodayRecipe()
     const categoryName = getCategoryName()
-    if (!todayRecipe) return <div>loading...</div>
+    // if (!todayRecipe) return <div>loading...</div>
     return (
         <div className="m-[16px]">
             <h1 className="mb-[16px] text-[32px] font-bold text-gray-700">
                 今日作る{categoryName}は・・・
             </h1>
-            <RecipeThumbnail foodImageUrl={todayRecipe.foodImageUrl} />
+            {/* <RecipeThumbnail foodImageUrl={todayRecipe.foodImageUrl} /> */}
             <div className="m-[8px]">
-                <RecipeTitle title={todayRecipe.recipeTitle} />
+                {/* <RecipeTitle title={todayRecipe.recipeTitle} /> */}
                 <RecipeDetailButton />
             </div>
         </div>
