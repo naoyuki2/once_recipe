03c95944d756eea27e9f2f0b05d742cd06dc7e1a
naoyuki <kamaboko912@gmail.com>
2024-02-19 09:47:00 +0900

    randを固定化

--- app/utils/recipe.ts
+++ app/utils/recipe.ts
@@ -1,13 +1,15 @@
 import { allCategory } from '../api/rakuten/categoryData'
 import { fetchRankingTop4ByCategoryId } from '../api/rakuten/dataFetch'
 
-const randomCategoryNumber = Math.floor(Math.random() * allCategory.length)
+// const randomCategoryNumber = Math.floor(Math.random() * allCategory.length)
+// const randomRecipeNumber = Math.floor(Math.random() * 4)
+const randomCategoryNumber = 0
+const randomRecipeNumber = 0
 
 export const getTodayRecipe = async () => {
     const ranking = await fetchRankingTop4ByCategoryId(
         allCategory[randomCategoryNumber].categoryId,
     )
-    const randomRecipeNumber = Math.floor(Math.random() * 4)
     const todayRecipe = ranking.result[randomRecipeNumber]
     return todayRecipe
 }
