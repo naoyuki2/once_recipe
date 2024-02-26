31a02bebd584a2067fded35e9f806325ca293771
naoyuki <kamaboko912@gmail.com>
2024-02-26 10:36:58 +0900

    revalidatePathで/ページを再検証した

--- utils/recipe/getTodayRecipe.ts
+++ utils/recipe/getTodayRecipe.ts
@@ -8,15 +8,15 @@ import { revalidatePath } from 'next/cache'
 
 export const getTodayRecipe = async (): Promise<Recipe> => {
     const archive = await getArchive()
-    const todayRecipe = await getRecipeById({ recipeId: archive.RecipeId })
-    if (todayRecipe.length === 0) {
+    console.log(archive)
+    if (archive.RecipeId === 0) {
         console.log('archiveはなかった')
         const newTodayRecipe = await fetchRakutenRecipe()
         const recipeId = await postRecipe({ recipe: newTodayRecipe })
         await postArchive(recipeId.Id)
-        revalidatePath('/api/archive')
+        revalidatePath('/', 'layout')
         return newTodayRecipe
-    } else {
-        return todayRecipe[0]
     }
+    const todayRecipe = await getRecipeById({ recipeId: archive.RecipeId })
+    return todayRecipe[0]
 }
